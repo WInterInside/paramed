0286d24847a0fe036471d3a66e3a90003e38b36d
Максим Циркулев <de_animator@mail.ru>
2025-02-14 20:22:31 +0300

    upd project

--- build/js/app.js
+++ build/js/app.js
@@ -14,15 +14,6 @@ const toggleNav = () => {
 	}
 };
 
-const toggleSubNav = () => {
-	if (!body.hasClass('is-sub-nav-open')) {
-		body.addClass('is-sub-nav-open');
-	} else {
-		body.removeClass('is-sub-nav-open');
-	}
-};
-
-
 // Обработка прокрутки страницы
 const handleScroll = () => {
 	if ($(window).scrollTop() > 0) {
@@ -34,7 +25,6 @@ const handleScroll = () => {
 
 // Событие клика на навигационном переключателе
 navToggle.on('click', toggleNav);
-subNavToggle.on('click', toggleSubNav);
 
 // Событие прокрутки окна
 $(window).on('scroll', handleScroll);
@@ -98,237 +88,6 @@ $(document).on('click', '[data-elements~="tabsBtn"]', function(e) {
 	});
 });
 
-$(document).ready(function () {
-	var sliders = $('.drawings__slider');
-	var prevButtons = $('.prev');
-	var nextButtons = $('.next');
-
-
-	sliders.each(function(index) {
-		var currentSlider = $(this);
-
-		currentSlider.slick({
-			dots: false,
-			arrows: false,
-			controls: false,
-			infinite: true,
-			speed: 300,
-			slidesToShow: 1,
-			adaptiveHeight: true,
-			autoplay: true,
-			autoplaySpeed: 3000
-		});
-
-
-		prevButtons.eq(index).on('click', function () {
-			currentSlider.slick('slickPrev');
-		});
-
-		nextButtons.eq(index).on('click', function () {
-			currentSlider.slick('slickNext');
-		});
-	});
-});
-
-$(document).ready(function() {
-	var $bigSlider = $('.big-slider');
-	var $smallSlider = $('.small-slider');
-
-	$bigSlider.slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		fade: true,
-		asNavFor: '.small-slider'
-	});
-
-	$smallSlider.slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		asNavFor: '.big-slider',
-		dots: false,
-		arrows: false,
-		centerMode: false,
-		focusOnSelect: true
-	});
-
-	// Кастомные кнопки управления
-	$('.prev').on('click', function() {
-		$smallSlider.slick('slickPrev');
-	});
-
-	$('.next').on('click', function() {
-		$smallSlider.slick('slickNext');
-	});
-});
-
-$(document).ready(function () {
-	// Находим все слайдеры и кнопки
-	var sliders = $('.drawings__slider');
-	var prevButtons = $('.prev');
-	var nextButtons = $('.next');
-
-	// Проходимся по каждому слайдеру
-	sliders.each(function(index) {
-		var currentSlider = $(this);
-
-		// Инициализируем текущий слайдер
-		currentSlider.slick({
-			dots: false,
-			arrows: false,
-			controls: false,
-			infinite: true,
-			speed: 300,
-			slidesToShow: 1,
-			adaptiveHeight: true,
-			autoplay: true,
-			autoplaySpeed: 3000
-		});
-
-		// Привязываем события к кнопкам текущего слайдера
-		prevButtons.eq(index).on('click', function () {
-			currentSlider.slick('slickPrev');
-		});
-
-		nextButtons.eq(index).on('click', function () {
-			currentSlider.slick('slickNext');
-		});
-	});
-});
-
-$(document).ready(function () {
-	var sliders = $('.project-page__slider');
-	var prevButtons = $('.prev');
-	var nextButtons = $('.next');
-
-	sliders.each(function(index) {
-	var currentSlider = $(this);
-
-	if (currentSlider.hasClass('presentation')) {
-		currentSlider.slick({
-			dots: true,
-			arrows: false,
-			controls: false,
-			infinite: true,
-			speed: 300,
-			slidesToShow: 3,
-			adaptiveHeight: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-			responsive: [
-			{
-				breakpoint: 1300,
-				settings: {
-				slidesToShow: 2
-				}
-			},
-			{
-				breakpoint: 900,
-				settings: {
-				slidesToShow: 1
-				}
-			}
-			]
-		});
-	} else {
-		currentSlider.slick({
-			dots: true,
-			arrows: false,
-			controls: false,
-			infinite: true,
-			speed: 300,
-			slidesToShow: 1,
-			adaptiveHeight: true,
-			autoplay: false,
-			autoplaySpeed: 3000
-		});
-	}
-
-	// Привязываем события к кнопкам текущего слайдера
-	prevButtons.eq(index).on('click', function () {
-		currentSlider.slick('slickPrev');
-	});
-
-	nextButtons.eq(index).on('click', function () {
-		currentSlider.slick('slickNext');
-	});
-	});
-});
-
-$(document).ready(function() {
-	// Функция для проверки видимости блока
-	function isElementInViewport(el) {
-		var rect = el.getBoundingClientRect();
-
-		return (
-		rect.top < window.innerHeight && // Верхняя граница блока выше нижней границы окна
-		rect.bottom > 0 &&               // Нижняя граница блока ниже верхней границы окна
-		rect.left < window.innerWidth && // Левая граница блока левее правой границы окна
-		rect.right > 0                   // Правая граница блока правее левой границы окна
-		);
-	}
-
-	// Функция для анимации
-	function animateDescriptionItems() {
-		const items = $('.description__item');
-
-		if (!items.length) { // Если блоков нет, то ничего не делаем
-			return;
-		}
-
-		let currentIndex = 0;
-
-		setInterval(() => {
-			items.removeClass('is-current'); // Убираем текущий класс у всех элементов
-
-			// Проверяем видимость текущего элемента
-			if (isElementInViewport(items[currentIndex])) {
-				items.eq(currentIndex).addClass('is-current'); // Добавляем класс текущему элементу
-
-				currentIndex++;
-				if (currentIndex >= items.length) {
-					currentIndex = 0;
-				}
-			}
-		}, 2000); // Переключаем каждые 2 секунды
-	}
-
-
-	// Функция для анимации
-	function animateDescriptionItemsSteps() {
-		const items = $('.map__step');
-
-		if (!items.length) { // Если блоков нет, то ничего не делаем
-			return;
-		}
-
-		let currentIndex = 0;
-
-		if ($(window).width() < 1300) {
-
-			setInterval(() => {
-				items.removeClass('is-current'); // Убираем текущий класс у всех элементов
-
-				// Проверяем видимость текущего элемента
-				if (isElementInViewport(items[currentIndex])) {
-					items.eq(currentIndex).addClass('is-current'); // Добавляем класс текущему элементу
-
-					currentIndex++;
-					if (currentIndex >= items.length) {
-						currentIndex = 0;
-					}
-				}
-			}, 2000); // Переключаем каждые 2 секунды
-
-		} else {
-			return;
-		}
-	}
-
-	// Запускаем анимацию при загрузке страницы
-	animateDescriptionItems();
-	animateDescriptionItemsSteps();
-});
 
 $(document).ready(function() {
 	if ($(".faq").length > 0) { // Проверяем наличие блока с классом "faq"
@@ -346,11 +105,11 @@ $(document).ready(function() {
 });
 
 $(document).ready(function () {
-	var $slider = $('.docs__slider');
+	var $slider = $('.reviews__slider');
 
 	// Инициализация слайдера
 	$slider.slick({
-		dots: false,
+		dots: true,
 		infinite: true,
 		speed: 500,
 		slidesToShow: 5,
@@ -387,118 +146,46 @@ $(document).ready(function () {
 	});
 });
 
-$(document).ready(function () {
-	// Находим все элементы с классом devices__item
-	$('.devices__item').each(function () {
-		// Находим первый элемент details внутри блока
-		var firstDetails = $(this).find('details')[0];
-
-		if (firstDetails) {
-			// Открываем первый элемент details
-			firstDetails.open = true;
-		}
-	});
-});
-
 $(document).ready(function() {
-    // Находим все элементы с классом map__data-item
-    var items = $('.map__data-item');
-    
-    function checkVisibility() {
-        items.each(function(index, item) {
-            if (isElementPartiallyVisible(item, 0.3)) { // Проверяем видимость на 30%
-                setTimeout(function() {
-                    $(item).addClass('is-visible');
-                }, index * 500); // Задержка в 1 секунду между каждым элементом
-            }
-        });
-    }
-
-    // Функция для проверки частичной видимости элемента на экране
-    function isElementPartiallyVisible(el, threshold) {
-        var rect = el.getBoundingClientRect();
-        
-        // Высота и ширина элемента
-        var elementHeight = rect.height || el.offsetHeight;
-        var elementWidth = rect.width || el.offsetWidth;
-        
-        // Видимая высота и ширина элемента
-        var visibleHeight = Math.max(0, window.innerHeight - rect.top);
-        var visibleWidth = Math.max(0, window.innerWidth - rect.left);
-        
-        // Процент видимости по высоте и ширине
-        var heightPercentage = visibleHeight / elementHeight;
-        var widthPercentage = visibleWidth / elementWidth;
-        
-        // Элемент считается частично видимым, если виден хотя бы на заданный процент
-        return (heightPercentage >= threshold && widthPercentage >= threshold);
-    }
-
-    // Запускаем проверку при загрузке страницы
-    checkVisibility();
-
-    // Также проверяем видимость элементов при прокрутке окна
-    $(window).on('scroll', checkVisibility);
-});
-
-$(document).ready(function () {
-    // Находим все элементы с классом .map__order
-    var orders = $('.map__order');
-
-    function checkOrderVisibility() {
-        orders.each(function (index, order) {
-            // Для каждого блока находим все вложенные div'ы
-            var childrenDivs = $(order).find('div');
-
-            childrenDivs.each(function (childIndex, childItem) {
-                if (isElementPartiallyVisible(childItem, 0.3)) { // Проверяем видимость на 30%
-                    setTimeout(function () {
-                        $(childItem).addClass('is-visible');
-                    }, childIndex * 1000); // Задержка в 1 секунду между каждым элементом
-                }
-            });
-        });
-    }
-
-    // Функция для проверки частичной видимости элемента на экране
-    function isElementPartiallyVisible(el, threshold) {
-        var rect = el.getBoundingClientRect();
-
-        // Высота и ширина элемента
-        var elementHeight = rect.height || el.offsetHeight;
-        var elementWidth = rect.width || el.offsetWidth;
-
-        // Видимая высота и ширина элемента
-        var visibleHeight = Math.max(0, window.innerHeight - rect.top);
-        var visibleWidth = Math.max(0, window.innerWidth - rect.left);
-
-        // Процент видимости по высоте и ширине
-        var heightPercentage = visibleHeight / elementHeight;
-        var widthPercentage = visibleWidth / elementWidth;
-
-        // Элемент считается частично видимым, если виден хотя бы на заданный процент
-        return (heightPercentage >= threshold && widthPercentage >= threshold);
-    }
-
-    // Запускаем проверку при загрузке страницы
-    checkOrderVisibility();
-
-    // Также проверяем видимость элементов при прокрутке окна
-    $(window).on('scroll', checkOrderVisibility);
-});
-
-// подсветка текущей станици в навигации - мб стоит подобрать другую реализацию, стили для класса написаны.
-$(document).ready(function () {
-	// Получаем текущий URL страницы
-	var currentUrl = window.location.pathname;
-
-	// Находим все ссылки внутри списка навигации
-	$('nav ul li a').each(function () {
-		var $this = $(this);
-
-		if ($this.attr('href') === currentUrl || $this.prop('href') === window.location.href) {
-			// Если ссылка совпадает с текущей страницей, добавляем ей класс is-current
-			$this.addClass('is-current');
-		}
+	$('.footer__up').on('click', function(event) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: 0 // Прокручиваем до верха страницы
+		}, 500); // Время анимации в миллисекундах (здесь 500 мс)
 	});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+	// Функция создания карты
+	function createMap(xID, coords) {
+		const mapContainer = document.getElementById(xID);
+		if (mapContainer) {
+			ymaps.ready(function() {
+				const myMap = new ymaps.Map(xID, {
+					center: coords,
+					zoom: 13,
+					controls: [],
+				});
+
+				const myPlacemark = new ymaps.Placemark(
+					coords,
+					{
+						hintContent: "г. Сочи улица Северная, д. 16",
+						balloonContent: "г. Сочи улица Северная, д. 16",
+					},
+					{
+						preset: "islands#redDotIcon", // Красный пин
+					}
+				);
+
+				myMap.geoObjects.add(myPlacemark);
+				myMap.behaviors.disable("scrollZoom");
+			});
+		} else {
+			console.error("Элемент с ID", xID, "не найден.");
+		}
+	}
+
+	// Вызов функции создания карты
+	createMap("contacts-page__map", [43.595573, 39.724697]);
 });
